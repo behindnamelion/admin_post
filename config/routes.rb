b8fc9418f28a9966076b51721e3d3011b71f9fbc
behindnamelion <behindname@likelion.org>
2020-05-14 17:14:44 +0000

    Add privateQuestion table by executing db migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :private_questions
+  
+  root 'private_questions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

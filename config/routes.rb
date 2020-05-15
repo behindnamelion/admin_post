b6ffcdf43364860b008dc52b6b49ab2362fbd27c
behindnamelion <behindname@likelion.org>
2020-05-15 06:56:54 +0000

    Add home controller for main page and Add navbar and panel UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   resources :private_questions
   
-  root 'private_questions#index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

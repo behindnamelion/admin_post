1eb8ba75ea4ca7fe4a077b73c1d1f9e8a9417b2d
behindnamelion <behindname@likelion.org>
2020-05-18 03:29:17 +0000

    Add extra fields to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_020003) do
+ActiveRecord::Schema.define(version: 2020_05_18_023438) do
 
   create_table "private_questions", force: :cascade do |t|
     t.string "title"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 2020_05_18_020003) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "company"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
